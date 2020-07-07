9544a69b6494feac27ef9351aff7d64ac5464ef9
Hemanth Krishna <hkpdev008@gmail.com>
2020-07-07 08:01:47 +0530

    wifip2p: feat:P2P Discovery

--- wifip2p/app/src/main/java/com/benrostudios/wifip2p/MainActivity.kt
+++ wifip2p/app/src/main/java/com/benrostudios/wifip2p/MainActivity.kt
@@ -1,11 +1,87 @@
 package com.benrostudios.wifip2p
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.IntentFilter
+import android.net.wifi.p2p.WifiP2pConfig
+import android.net.wifi.p2p.WifiP2pDevice
+import android.net.wifi.p2p.WifiP2pDeviceList
+import android.net.wifi.p2p.WifiP2pManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    companion object{
+        const val TAG = "MainActivity"
+    }
+    val manager: WifiP2pManager? by lazy(LazyThreadSafetyMode.NONE) {
+        getSystemService(Context.WIFI_P2P_SERVICE) as WifiP2pManager?
+    }
+
+    var channel: WifiP2pManager.Channel? = null
+    var receiver: BroadcastReceiver? = null
+    val intentFilter = IntentFilter().apply {
+        addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION)
+        addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION)
+        addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION)
+        addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        channel = manager?.initialize(this, mainLooper, null)
+        channel?.also { channel ->
+            receiver = WiFiDirectBroadcastReceiver(manager!!, channel, this)
+        }
+        button.setOnClickListener {
+            manager?.discoverPeers(channel, object : WifiP2pManager.ActionListener {
+
+                override fun onSuccess() {
+                    Log.d(TAG,"Found Peers!")
+                }
+
+                override fun onFailure(reasonCode: Int) {
+                    Log.d(TAG,"Didn't Find any peers!")
+                }
+            })
+
+        }
+
+
+    }
+    override fun onResume() {
+        super.onResume()
+        receiver?.also { receiver ->
+            registerReceiver(receiver, intentFilter)
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        receiver?.also { receiver ->
+            unregisterReceiver(receiver)
+        }
+    }
+
+    private fun connectToPeer(wifiP2pDevice: WifiP2pDevice){
+        val device: WifiP2pDevice = wifiP2pDevice
+        val config = WifiP2pConfig()
+        config.deviceAddress = device.deviceAddress
+        channel?.also { channel ->
+            manager?.connect(channel, config, object : WifiP2pManager.ActionListener {
+
+                override fun onSuccess() {
+                    Log.d(TAG,"Connected To Peer!!")
+                }
+
+                override fun onFailure(reason: Int) {
+                    Log.d(TAG,"Failure Connecting to peer!")
+                }
+            }
+            )}
     }
 }
--- wifip2p/app/src/main/java/com/benrostudios/wifip2p/WiFiDirectBroadcastReceiver.kt
+++ wifip2p/app/src/main/java/com/benrostudios/wifip2p/WiFiDirectBroadcastReceiver.kt
@@ -3,7 +3,9 @@ package com.benrostudios.wifip2p
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.net.wifi.p2p.WifiP2pDeviceList
 import android.net.wifi.p2p.WifiP2pManager
+import android.util.Log
 
 class WiFiDirectBroadcastReceiver (
     private val manager: WifiP2pManager,
@@ -15,9 +17,19 @@ class WiFiDirectBroadcastReceiver (
         when (intent.action) {
             WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION -> {
                 // Check to see if Wi-Fi is enabled and notify appropriate activity
+                when (intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1)) {
+                    WifiP2pManager.WIFI_P2P_STATE_ENABLED -> {
+                        // Wifi P2P is enabled
+                    }
+                    else -> {
+                        // Wi-Fi P2P is not enabled
+                    }
+                }
             }
             WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION -> {
-                // Call WifiP2pManager.requestPeers() to get a list of current peers
+                manager.requestPeers(channel) {
+                    Log.d("P2P DEVICES","$it")
+                }
             }
             WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION -> {
                 // Respond to new connection or disconnections
